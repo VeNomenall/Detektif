4599f15423d5c9141612a5c59e5b370e3eb3583c
VeNomenall <162926027+VeNomenall@users.noreply.github.com>
2024-03-11 05:31:59 +0700

    unzipgit

---
+++ github.com/VeNomenall/Detektif/Mr.Holmes-T.G.D-1.0.2/GUI/Actions/Email_Finder.php
@@ -0,0 +1,97 @@
+<?php
+    /*ORIGINAL CREATOR: Luca Garofalo (Lucksi)
+    AUTHOR: Luca Garofalo (Lucksi)
+    Copyright (C) 2021-2023 Lucksi <lukege287@gmail.com>
+    License: GNU General Public License v3.0*/
+
+    function Get_Message($Type,$Param){
+        require_once ("Language_Controller.php");
+        $Message = Message($Type,$Param);
+        return $Message;
+    }
+    
+    function get_dorks($Complete_name){
+        if(file_exists($Complete_name)){
+            echo "<div class = 'Dataf'>";
+            echo "<p id = 'Const'>DORKS:</p>";
+            $data = fopen($Complete_name,"r")or die("Sever-Error");
+            while (!feof($data)){
+                $content = fgets($data);
+                echo "<p>".$content;
+            }
+            fclose($data);
+            echo "</p>";
+            echo "\n</div>";     
+        }
+        else {
+            echo "\n\t\t\t<p id = 'error' align = 'center'>NOT FIND ANY DORK FOR THIS EMAIL</p>";
+        }
+    }
+    
+    function Checker() {
+        $File_name = $_POST["Searcher"];
+        if ($File_name == "") {
+            $Message = Get_Message("NotEntered","Email");
+            echo "
+            <script>
+            alert('$Message');
+            </script>";
+        }
+        else {
+            $Complete_name = "../Reports/E-Mail/{$File_name}.txt";
+            $Complete_name2 = "../Reports/E-Mail/{$File_name}.mh";
+            if(file_exists($Complete_name)){
+                $Message = Get_Message("Positives","Email");
+                echo "
+                <script>
+                alert('$Message');
+                </script>";
+                echo "<p id = 'Const'>EMAIL DATA</p>";
+                echo "<div class = 'Datap'>";
+                echo "<p id = 'Const'>REPORT:</p>";
+                $data = fopen($Complete_name,"r")or die("Sever-Error");
+                while (!feof($data)){
+                    $content = fgets($data);
+                    echo "<p>".$content;
+                }
+                fclose($data);
+                echo "</p>";
+                echo "\n</div>";
+                $Complete_name = "../Reports/E-Mail/Dorks/{$File_name}_dorks.txt";
+                get_dorks($Complete_name);
+            }
+            else if(file_exists($Complete_name2)){
+                require_once("Decode.php");
+                $Message = Get_Message("Positives","Email");
+                echo "
+                <script>
+                alert('$Message');
+                </script>";
+                echo "<p id = 'Const'>EMAIL DATA</p>";
+                echo "<div class = 'Datap'>";
+                echo "<p id = 'Const'>REPORT:</p>";
+                $data = fopen($Complete_name2,"r")or die("Sever-Error");
+                while (!feof($data)){
+                    $content = fgets($data);
+                    $string = Decode($content);
+                    echo "<pre><p>".$string."</p></pre>";
+                }
+                fclose($data);
+                echo "</p>";
+                echo "\n</div>";
+                $Complete_name = "../Reports/E-Mail/Dorks/{$File_name}_dorks.txt";
+                get_dorks($Complete_name);
+            }
+            else {
+                $Message = Get_Message("Errors","Email");
+                echo "
+                <script>
+                alert('$Message');
+                </script>";
+            }
+        }
+    }
+    if(isset($_POST["Button"])){
+        Checker();
+    }
+?>
